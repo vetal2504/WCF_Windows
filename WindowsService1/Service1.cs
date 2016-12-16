6f3298b3071ad7be1e082e03f2af2cd14566b937
vetal2504 <pandawert@gmail.com>
2016-12-16 16:24:31 +0200

    Do not install service

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteKeyboard write = new WriteKeyboard();
+            Thread loggerThread = new Thread(new ThreadStart(write.Work));
+            loggerThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ WindowsService1/WriteKeyboard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsService1
+{
+    class WriteKeyboard
+    {
+        private bool write = true;
+        public WriteKeyboard()
+        {
+            //Work();
+        }
+
+        public bool Write
+        {
+            get
+            {
+                return write;
+            }
+            set
+            {
+                write = value;
+            }
+        }
+
+        public void Work()
+        {
+            //string path = Directory.GetCurrentDirectory();
+            //FileInfo file = new FileInfo("C:\\Windiws\\file.txt");
+            using (StreamWriter sw = new StreamWriter(@"C:\Windows\file.txt", true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine("\\n");
+                sw.Write(Console.ReadLine());
+            }
+        }
+    }
+}
